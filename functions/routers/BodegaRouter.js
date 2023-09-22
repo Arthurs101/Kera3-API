12c0a452e622d5956704074e1793c5941f0bf63d
Arthurs101 <84546401+Arthurs101@users.noreply.github.com>
2023-09-21 19:57:06 -0600

    Deleted Unused files

--- functions/routers/BodegaRouter.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const router = new Router();
-const controller = require('../controllers/BodegaController');
-//get--methods
-router.get('/', controller.getManager);
-
-//post--methods
-module.exports = router
